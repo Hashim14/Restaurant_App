eb991882723e169b15446ad41ebcd901a4765396
shaikalavudeen <shaik001meeran@gmail.com>
2021-07-24 12:39:28 +0530

    * Add a new file to the component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react"; 
 import "./App.css";
 import NavArea from "./Components/navArea";
+import Card from "./Components/card";
 
-const App = () => <div className="App"><NavArea /></div>;
+
+const App = () => <div className="App"><NavArea /><Card/></div>;
 
 export default App;
  
