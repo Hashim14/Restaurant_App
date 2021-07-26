feb3f3b08ca315b37ae799e8e17c33bbc09694a9
Mohammed Hashim <mohdkhashim07@gmail.com>
2021-07-26 15:50:20 +0530

    Title added Navbar created

--- src/Components/navArea.tsx
+++ src/Components/navArea.tsx
@@ -1,17 +1,45 @@
+import React from "react";
 import { Input, Layout, Menu } from "antd";
 import { Header } from "antd/lib/layout/layout";
+import Modal from "antd/lib/modal/Modal";
 
 const NavArea = () => {
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => {
+    setVisible(true);
+  };
+  const closeModal = () => {
+    setVisible(false);
+  };
+
+  const onOkay = () => {
+    setVisible(false);
+  };
+
   return (
     <>
       <Layout>
         <Header className="header">
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
             <Menu.Item key="1">Home</Menu.Item>
-            <Input style={{width: "200px"}} placeholder="Search Food,Restaurant" />
             <Menu.Item key="2">Filter</Menu.Item>
-            <Menu.Item key="3">Add Restaurant</Menu.Item>
+            <Menu.Item key="3" onClick={showModal}>
+              Add Restaurant
+            </Menu.Item>
+            <Input
+              style={{ width: "200px" }}
+              placeholder="Search Food,Restaurant"
+            />
+            <Modal
+              title="Create Restaurant"
+              visible={visible}
+              onCancel={closeModal}
+              onOk={onOkay}
+            >
+              hi
+            </Modal>
           </Menu>
         </Header>
       </Layout>
