87eb9008479eae60ea678ff64c0c7c8a619eb05d
Mohammed Hashim <mohdkhashim07@gmail.com>
2021-07-27 11:46:54 +0530

    removed card.tsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react"; 
+import React from "react";
 import "./App.css";
 import NavArea from "./Components/navArea";
-import Card from "./Components/card";
 
-
-const App = () => <div className="App"><NavArea /><Card/></div>;
+const App = () => (
+  <div className="App">
+    <NavArea />
+  </div>
+);
 
 export default App;
- 
